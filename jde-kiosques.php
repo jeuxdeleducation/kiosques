dcc36ea07f8600ed40c03415ae2f5559e38bb8ec
Samuel Lavoie <admin.samuel@jeuxdeleducation.com>
2025-03-06 22:36:24 -0500

    Update jde-kiosques.php

--- jde-kiosques.php
+++ jde-kiosques.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: JDE Kiosques
 Description: Plugin de gestion des kiosques pour Jeux de l'Éducation.
-Version: 1.2.3
+Version: 1.2.4
 Author: Samuel Lavoie
 Author URI: https://github.com/jeuxdeleducation
 License: GPL2
