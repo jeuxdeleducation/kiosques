a541a5e9becb12c088e952aa2f69c983228b0e2a
Samuel Lavoie <admin.samuel@jeuxdeleducation.com>
2025-03-06 21:43:22 -0500

    Update class-settings.php

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -5,12 +5,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class JDE_Kiosques_Settings {
     public static function settings_page() {
-        // Seuls les administrateurs peuvent accéder à cette page.
         if ( ! current_user_can( 'manage_options' ) ) {
             wp_die( __( 'Accès refusé.', 'jde-kiosques' ) );
         }
         
-        // Traitement du formulaire
         if ( isset( $_POST['jde_kiosques_settings_nonce'] ) && wp_verify_nonce( $_POST['jde_kiosques_settings_nonce'], 'jde_kiosques_save_settings' ) ) {
             $total = isset( $_POST['total_kiosques'] ) ? intval( $_POST['total_kiosques'] ) : 20;
             update_option( 'jde_kiosques_total', $total );
@@ -18,20 +16,19 @@ class JDE_Kiosques_Settings {
             $restrict = isset( $_POST['restrict_access'] ) ? 1 : 0;
             update_option( 'jde_kiosques_restrict_access', $restrict );
             
-            // Utilisation du type URL pour une validation native HTML5
             $plan_image = isset( $_POST['plan_image'] ) ? esc_url_raw( $_POST['plan_image'] ) : '';
             update_option( 'jde_kiosques_plan_image', $plan_image );
             
-            echo '<div class="updated notice"><p>' . __( 'Paramètres mis à jour.', 'jde-kiosques' ) . '</p></div>';
+            add_settings_error( 'jde_kiosques_settings', 'settings_updated', __( 'Paramètres mis à jour.', 'jde-kiosques' ), 'updated' );
         }
         
-        // Récupération des valeurs enregistrées
         $total_kiosques  = intval( get_option( 'jde_kiosques_total', 20 ) );
         $restrict_access = intval( get_option( 'jde_kiosques_restrict_access', 0 ) );
         $plan_image      = esc_url( get_option( 'jde_kiosques_plan_image', '' ) );
         ?>
         <div class="wrap">
             <h1><?php _e( 'Configuration JDE Kiosques', 'jde-kiosques' ); ?></h1>
+            <?php settings_errors( 'jde_kiosques_settings' ); ?>
             <form method="post" action="">
                 <?php wp_nonce_field( 'jde_kiosques_save_settings', 'jde_kiosques_settings_nonce' ); ?>
                 <table class="form-table">
@@ -42,17 +39,17 @@ class JDE_Kiosques_Settings {
                         </td>
                     </tr>
                     <tr valign="top">
-                        <th scope="row"><?php _e( 'Restreindre l\'accès aux pages du plugin aux organisateurs uniquement', 'jde-kiosques' ); ?></th>
+                        <th scope="row"><?php _e( 'Restreindre l'accès aux organisateurs uniquement', 'jde-kiosques' ); ?></th>
                         <td>
                             <input type="checkbox" name="restrict_access" value="1" <?php checked( $restrict_access, 1 ); ?> />
                         </td>
                     </tr>
                     <tr valign="top">
-                        <th scope="row"><?php _e( 'URL de l\'image du plan', 'jde-kiosques' ); ?></th>
+                        <th scope="row"><?php _e( 'URL de l'image du plan', 'jde-kiosques' ); ?></th>
                         <td>
                             <input type="url" name="plan_image" value="<?php echo esc_attr( $plan_image ); ?>" style="width: 60%;" />
                             <p class="description">
-                                <?php _e( 'Entrez l\'URL de l\'image représentant le plan. Exemple : https://votresite.com/wp-content/uploads/plan.jpg', 'jde-kiosques' ); ?>
+                                <?php _e( 'Entrez l'URL de l'image représentant le plan.', 'jde-kiosques' ); ?>
                             </p>
                         </td>
                     </tr>
